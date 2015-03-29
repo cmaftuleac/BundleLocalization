4bf24f23660e3d63019a9a3b224de2771ae08514
Cornel <kernel.md@gmail.com>
2015-03-28 17:04:30 -0700

    Files upload

---
+++ BundleLocalization.m
@@ -0,0 +1,77 @@
+//    The MIT License (MIT)
+//
+//    Copyright (c) 2015 Corneliu Maftuleac
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
+
+#import "BundleLocalization.h"
+#import "NSBundle+Localization.h"
+
+
+@implementation BundleLocalization
+{
+    NSString* selectedLanguage;
+}
+
++ (BundleLocalization*) sharedInstance {
+    @synchronized(self) {
+        static BundleLocalization* instance = nil;
+        if (instance == nil) {
+            instance = [[BundleLocalization alloc] init];
+        }
+        return instance;
+    }
+}
+
+- (id) init {
+    self = [super init];
+    if (self) {
+        // use systems main bundle as default bundle
+        _localizationBundle = [NSBundle mainBundle];
+    }
+    return self;
+}
+
+- (void) setLanguage:(NSString*) lang {
+    // path to this languages bundle
+    NSString *path = [[NSBundle mainBundle] pathForResource:lang ofType:@"lproj" ];
+    if (path == nil) {
+        // there is no bundle for that language
+        // use main bundle instead
+        _localizationBundle = [NSBundle mainBundle];
+    } else {
+        // use this bundle as my bundle from now on:
+        _localizationBundle = [NSBundle bundleWithPath:path];
+        // to be absolutely sure (this is probably unnecessary):
+        if (_localizationBundle == nil) {
+            _localizationBundle = [NSBundle mainBundle];
+        }
+        selectedLanguage = lang;
+    }
+}
+
+- (NSString*) language{
+    if(_localizationBundle == [NSBundle mainBundle]) {
+        return [[NSBundle mainBundle] preferredLocalizations].firstObject;
+    }else {
+        return selectedLanguage;
+    }
+}
+
+@end
--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # BundleLocalization
 Workaround for XCode base internationalization to support on-the-fly language change
+
+## Reasoning
+
+Base internationalization is awesome, but sometime not enough.
+Sometimes users want to change the language inside an application, rathen than changing on the whole device.
+Currently there is no way to change the bundle language while the application is running.
+
+These set of classes are here to do just that!
+
+## Usage
+
+1) Drop the files to your project:
+
+    BundleLocalization.h
+    BundleLocalization.m
+    NSBundle+Localization.h
+    NSBundle+Localization.m
+
+2) Just import and use it
+
+    #import "BundleLocalization.h"
+    ...
+    [[BundleLocalization sharedInstance] setLanguage:@"fr"];
+
+or
+
+    [BundleLocalization sharedInstance].language = @"de";
+    NSLog(@"Application language: %@", [BundleLocalization sharedInstance].language);
+
+Nothing more is required, you can use NSLocalizedString() as usual.
+
+
+## Notes
+
+Remember that for the nib/storyboards to be translated you need to reload them.
+
+
+## Implementation details
+
+This implementation works by tweaking inside NSBundle.
+The idea is that you override the method localizedStringForKey on the instance of NSBundle object, and then call this method on a different bundle with a different language. 
+Simple and elegant fully compatible with all types of resources.
+
+## Lcence
+
+MIT Licence.
